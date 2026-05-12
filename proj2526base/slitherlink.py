3d81600851576041acbdabac31de4e5a22de0f46
marques-jpg <gfelicianomarques@gmail.com>
2026-05-12 16:09:24 +0100

    feat: problem modeling

--- proj2526base/slitherlink.py
+++ proj2526base/slitherlink.py
@@ -7,8 +7,9 @@
 # 113868 Guilherme Marques
 # 110126 António Correia
 
+import sys
 import random, copy
-from sys import stdin
+#from sys import stdin
 from collections import defaultdict
 
 import utils
@@ -22,124 +23,180 @@ from search import (
     depth_first_tree_search,
     greedy_search,
     recursive_best_first_search,
+    depth_first_graph_search,
 )
 
-
 class SlitherlinkState:
-    state_id = 0
-
-
-    def __init__(self, board):
+    def __init__(self, board, last_vertex=None):
         self.board = board
-        self.id = SlitherlinkState.state_id
-        SlitherlinkState.state_id += 1
-    
-    def __lt__(self, other):
-        return self.id < other.id
+        self.last_vertex = last_vertex
 
-    # TODO: outros metodos da classe
+    def __lt__(self, other):
+        return False
 
 class Board:
-    """Representação interna de um tabuleiro de Slitherlink."""
-    def __init__(self, clue):
-        self.clue = clue
-        N = len(clue)
-        M = len(clue[0])
+    def __init__(self, clues):
+        self.clues = clues
+        self.N = len(clues)
+        self.M = len(clues[0])
+        self.h_edges = [[0 for _ in range(self.M)] for _ in range(self.N + 1)]
+        self.v_edges = [[0 for _ in range(self.M + 1)] for _ in range(self.N)]
 
-        self.h_edges = [[0 for _ in range(M)] for _ in range(N+1)]
-        self.v_edges = [[0 for _ in range(M+1)] for _ in range(N)]
-
-    def adjacent_cell(self, cell:tuple) -> list:
-        """Devolve uma lista das células que fazem
-        fronteira com a célula enviada no argumento"""
-        #TODO
-        pass
-
-    def get_cell_edges(self, row:int, column:int) -> list:
+    def get_cell_edges(self, row: int, col: int) -> list:
         return [
-            ('h', row, column),
-            ('h', row + 1, column),
-            ('v', row, column),
-            ('v', row, column + 1)
+            ('h', row, col), ('h', row + 1, col),
+            ('v', row, col), ('v', row, col + 1)
         ]
 
-    def get_active_edges(self, row:int, column:int) -> list:
-        cell_edges = self.get_cell_edges(self, row, column)
+    def get_active_edges(self, row: int, col: int) -> int:
         count = 0
-        for typ, r, c in cell_edges:
-            if typ == 'h':
-                if self.h_edges[r][c] == 1: 
-                    count += 1
-            elif typ == 'v':
-                if self.v_edges[r][c] == 1:
-                    count += 1
+        for t, r, c in self.get_cell_edges(row, col):
+            val = self.h_edges[r][c] if t == 'h' else self.v_edges[r][c]
+            if val == 1: count += 1
         return count
+
+    def copy(self):
+        new_board = Board(self.clues)
+        new_board.h_edges = [linha[:] for linha in self.h_edges]
+        new_board.v_edges = [linha[:] for linha in self.v_edges]
+        return new_board
 
     @staticmethod
     def parse_instance():
-        import sys
         clues_temp = []
         for line in sys.stdin:
             value = line.split()
-            if value:
-                clues_temp.append(value)
-        clues_tuple = tuple(tuple(line) for line in clues_temp)
-        return Board(clues_tuple)
+            if value: clues_temp.append(value)
+        return Board(tuple(tuple(l) for l in clues_temp))
 
-    # TODO: outros metodos da classe
+    def display(self):
+        for r in range(self.N + 1):
+            h_line = ""
+            for c in range(self.M):
+                h_line += "."
+                h_line += "---" if self.h_edges[r][c] == 1 else "   "
+            h_line += "."
+            print(h_line)
+            
+            if r < self.N:
+                v_line = ""
+                for c in range(self.M + 1):
+                    v_line += "|" if self.v_edges[r][c] == 1 else " "
+                    if c < self.M:
+                        v_line += f" {self.clues[r][c]} "
+                print(v_line)
 
 class Slitherlink(Problem):
-    def __init__(self, board: Board, gui=None):
-        """O construtor especifica o estado inicial."""
-        # TODO
-        pass
-
+    def __init__(self, board: Board):
+        super().__init__(SlitherlinkState(board))
 
     def actions(self, state: SlitherlinkState):
-        """Retorna uma lista de ações que podem ser executadas a
-        partir do estado passado como argumento."""
-        # TODO
-        pass
+        board = state.board
+        if state.last_vertex is None:
+            r, c = self._find_best_starting_cell(board)
+            return board.get_cell_edges(r, c)
 
+        v_row, v_col = state.last_vertex
+        candidates = self._get_edges_touching_vertex(board, v_row, v_col)
+        
+        valid_actions = []
+        for action in candidates:
+            t, r, c = action
+            val = board.h_edges[r][c] if t == 'h' else board.v_edges[r][c]
+            if val != 0: continue
+            
+            if self._is_consistent(board, action, state.last_vertex):
+                valid_actions.append(action)
+        return valid_actions
 
     def result(self, state: SlitherlinkState, action):
-        """Retorna o estado resultante de executar a 'action' sobre
-        'state' passado como argumento. A ação a executar deve ser uma
-        das presentes na lista obtida pela execução de
-        self.actions(state)."""
-        # TODO
-        pass
+        new_board = state.board.copy()
+        t, r, c = action
+        if t == 'h': new_board.h_edges[r][c] = 1
+        else: new_board.v_edges[r][c] = 1
+        
+        v_dest = self._get_destination(action, state.last_vertex)
+        return SlitherlinkState(new_board, v_dest)
+
+    def _get_destination(self, action, last_v):
+        t, r, c = action
+        if last_v is None: return (r, c)
+        if t == 'h':
+            return (r, c) if (r, c+1) == last_v else (r, c+1)
+        return (r, c) if (r+1, c) == last_v else (r+1, c)
+
+    def _find_best_starting_cell(self, board):
+        for p in ['3', '2', '1']:
+            for r in range(board.N):
+                for c in range(board.M):
+                    if board.clues[r][c] == p: return (r, c)
+        return (0, 0)
+
+    def _get_edges_touching_vertex(self, board, row, col):
+        edges = []
+        if col > 0: edges.append(('h', row, col - 1))
+        if col < board.M: edges.append(('h', row, col))
+        if row > 0: edges.append(('v', row - 1, col))
+        if row < board.N: edges.append(('v', row, col))
+        return edges
+
+    def _is_consistent(self, board, action, last_v):
+        t, r, c = action
+        cells = []
+        if t == 'h':
+            if r > 0: cells.append((r - 1, c))
+            if r < board.N: cells.append((r, c))
+        else:
+            if c > 0: cells.append((r, c - 1))
+            if c < board.M: cells.append((r, c))
+
+        for cr, cc in cells:
+            clue = board.clues[cr][cc]
+            if clue != '.':
+                if board.get_active_edges(cr, cc) + 1 > int(clue):
+                    return False
+
+        v_dest = self._get_destination(action, last_v)
+        if self._count_active_at_vertex(board, v_dest) >= 2:
+            return False
+            
+        return True
+
+    def _count_active_at_vertex(self, board, vertex):
+        r, c = vertex
+        count = 0
+        for t, ar, ac in self._get_edges_touching_vertex(board, r, c):
+            val = board.h_edges[ar][ac] if t == 'h' else board.v_edges[ar][ac]
+            if val == 1: count += 1
+        return count
 
     def goal_test(self, state: SlitherlinkState):
-        """Retorna True se e só se o estado passado como argumento é
-        um estado objetivo. Deve verificar se todas as posições do tabuleiro
-        estão preenchidas de acordo com as regras do problema."""
-        # TODO
-        pass
-
-    def h(self, node: Node):
-        """Função heuristica utilizada para a procura A*."""
-        # TODO
-        pass
-
-    
-
+        board = state.board
+        
+        for r in range(board.N):
+            for c in range(board.M):
+                clue = board.clues[r][c]
+                if clue != '.' and board.get_active_edges(r, c) != int(clue):
+                    return False
+        for r in range(board.N + 1):
+            for c in range(board.M + 1):
+                grau = self._count_active_at_vertex(board, (r, c))
+                
+                if grau != 0 and grau != 2:
+                    return False
+        return True
 
 if __name__ == '__main__':
-    meu_board = Board.parse_instance()
+    board = Board.parse_instance()
     
-    print("--- Clues Read ---")
-    for linha in meu_board.clue:
-        print(linha)
-        
-    print("\n--- Internal Dimensions ---")
-    print(f"Clues Matrixs   : {len(meu_board.clue)} lines x {len(meu_board.clue[0])} columns")
-    print(f"h_edges Matrix  : {len(meu_board.h_edges)} lines x {len(meu_board.h_edges[0])} columns")
-    print(f"v_edges Matrix  : {len(meu_board.v_edges)} lines x {len(meu_board.v_edges[0])} columns")
-
-
-
-
-
-
-
+    problem = Slitherlink(board)
+    
+    print("A procurar solução...")
+    solucao_node = depth_first_tree_search(problem)
+    
+    if solucao_node:
+        print("Sucesso! Solução encontrada.")
+        final_board = solucao_node.state.board
+        solucao_node.state.board.display()
+    else:
+        print("Não foi encontrada nenhuma solução.")
